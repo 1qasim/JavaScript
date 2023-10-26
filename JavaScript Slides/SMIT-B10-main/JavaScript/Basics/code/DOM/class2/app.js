c92bd2021083b12ac070116c23172e24956cb15a
qasim <mq5613523@gmail.com>
2023-10-26 22:38:58 +0500

    some file setup

--- JavaScript Slides/SMIT-B10-main/JavaScript/Basics/code/DOM/class2/app.js
+++
@@ -1,8 +0,0 @@
-var userName = prompt("What is your name")
-var myName = document.getElementById('myName')
-
-console.log(myName, "===>>myName")
-
-if(userName.toLowerCase() === "sufiyan") {
-    document.body.style.backgroundColor = 'black'
-}
